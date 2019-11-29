7e01c7873c4040fb3efcc93e48807fde06e2f847
shikya <shrikantsonone@gmail.com>
2019-11-29 20:44:16 +0530

    prod  Config updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBrUL5GWjvG2a5p0AWXLxRvVLyviw5vnXk',
+    authDomain: 'bookish-pancake.firebaseapp.com',
+    databaseURL: 'https://bookish-pancake.firebaseio.com',
+    projectId: 'bookish-pancake',
+    storageBucket: 'bookish-pancake.appspot.com',
+    messagingSenderId: '50367786835',
+    appId: '1:50367786835:web:4b9b38ce350abc0b3f3ba6'
+  }
 };
