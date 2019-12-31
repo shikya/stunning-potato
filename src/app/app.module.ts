04fa147db603e324e78212e305001f5b029fddda
shikya <shrikantsonone@gmail.com>
2019-12-31 20:41:07 +0530

    Refactored code to make modules, upgraded jasmine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-// Materials
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { UserLoginComponent } from './users/user-login/user-login.component';
-import { UserProfileComponent } from './users/user-profile/user-profile.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // Custom
 import { AuthService } from './core/auth.service';
@@ -28,21 +14,19 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+// small modules
+import { RoutingModule } from './small-modules/routing.module';
+import { MatModulesModule } from './small-modules/mat-modules.module';
+
 import { EntryCardComponent } from './entry-card/entry-card.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './auth.guard';
 import { CreateEntryComponent } from './create-entry/create-entry.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { NormalUserGuard } from './normal-user.guard';
 import { NewUserComponent } from './new-user/new-user.component';
-
-const appRoutes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthGuard, NormalUserGuard]  },
-  { path: 'login', component: LoginComponent },
-  { path: 'newuser', component: NewUserComponent }
-];
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -57,27 +41,15 @@ const appRoutes = [
   ],
   imports: [
     BrowserModule,
+    MatModulesModule,
     AngularFireModule.initializeApp(environment.firebase, 'bookish-pancake'),
     BrowserAnimationsModule,
-    MatToolbarModule,
     AngularFireAuthModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatExpansionModule,
-    MatDividerModule,
-    MatInputModule,
     ReactiveFormsModule,
-    MatAutocompleteModule,
     FlexLayoutModule,
     AngularFirestoreModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    MatDialogModule,
-    MatProgressSpinnerModule
+    RoutingModule,
   ],
   entryComponents: [
     CreateEntryComponent
